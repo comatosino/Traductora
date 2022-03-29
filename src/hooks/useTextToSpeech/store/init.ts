f0a9bc6ecee96a8633d845e8cb3be0c592e50bc5
Robert Adams <adamsiii.robert@gmail.com>
2022-03-28 22:57:29 -0700

    added short circuit to avoid instantiating TTS when not supported

--- src/hooks/useTextToSpeech/store/init.ts
+++ src/hooks/useTextToSpeech/store/init.ts
@@ -3,7 +3,10 @@ import { TextToSpeechState } from "../types";
 
 const INITIAL_STATE: TextToSpeechState = {
   textToSpeech:
-    (TextToSpeech.isSupported() && TextToSpeech.getInstance()) || null,
+    (
+      TextToSpeech.isSupported()
+      && TextToSpeech.getInstance()
+    ) || null,
   speaking: false,
   language: navigator.language,
   selectedVoice: null,
