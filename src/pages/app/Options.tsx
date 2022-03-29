6678669d2db358fb00075f40ad0b6bff1d19d922
Robert Adams <adamsiii.robert@gmail.com>
2022-03-28 22:59:15 -0700

    removed alt voice options for now

--- src/pages/app/Options.tsx
+++ src/pages/app/Options.tsx
@@ -1,29 +1,13 @@
-import {
-  Divider,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-  Slider,
-  Stack,
-  Typography,
-} from "@mui/material";
-
+import React from "react";
+import { Divider, Slider, Stack, Typography } from "@mui/material";
 import {
   TextToSpeechOptions,
   TextToSpeechActions,
-  SpeechSynthesisVoiceMap,
 } from "../../hooks/useTextToSpeech/types";
 import { SpeechToTextOptions } from "../../hooks/useSpeechToText/types";
-
-import languages from "../../utils/maps/languages.json";
-import countries from "../../utils/maps/countries.json";
-
 import {
   setPitch,
   setRate,
-  setSelectedVoice,
   setVolume,
 } from "../../hooks/useTextToSpeech/store/actions";
 import { splitLangTag } from "../../utils";
@@ -31,15 +15,8 @@ import { splitLangTag } from "../../utils";
 const Options: React.FC<{
   micOptions: SpeechToTextOptions;
   speakOptions: TextToSpeechOptions;
-  getVoices: () => SpeechSynthesisVoiceMap | undefined;
-}> = ({ micOptions, speakOptions, getVoices }): JSX.Element => {
-  const altVoices = getVoices()![speakOptions.language];
+}> = ({ micOptions, speakOptions }): JSX.Element => {
   const [trgLangCode, trgCountryCode] = splitLangTag(speakOptions.language);
-
-  const handleSetVoice = (e: SelectChangeEvent<string>) => {
-    const voice = altVoices.find((voice) => voice.name === e.target.value);
-    if (voice) speakOptions.dispatch(setSelectedVoice(voice));
-  };
 
   return (
     <Stack
@@ -53,25 +30,6 @@ const Options: React.FC<{
       <Typography component="h2">
         <Divider>Voice Options</Divider>
       </Typography>
-
-      <FormControl>
-        <InputLabel>{`${languages[trgLangCode]["exonym"]["en"]} (${countries[trgCountryCode]}) Alternate Voices`}</InputLabel>
-        <Select
-          labelId="alt-voice-select-label"
-          id="alt-voice-select"
-          value={speakOptions.selectedVoice?.name || ""}
-          label={`${languages[trgLangCode]["exonym"]["en"]} (${countries[trgCountryCode]}) Alternate Voices`}
-          onChange={handleSetVoice}
-        >
-          {altVoices.map((voice) => {
-            return (
-              <MenuItem key={voice.name} value={voice.name}>
-                {voice.name}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
 
       <Typography id="volume-slider">Volume</Typography>
       <Slider
